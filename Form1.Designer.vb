b8804d7bb608d01874f59f0d93e059eece9b8b06
Lenovo-User <Lenovo-User@DESKTOP-T3K84PN>
2024-01-18 06:28:07 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        ComboBox1 = New System.Windows.Forms.ComboBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(38, 266)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(111, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Add Item"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(176, 266)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(131, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Remove Item"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(332, 266)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(144, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "Remove selected"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(537, 266)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(123, 23)
+        Button4.TabIndex = 3
+        Button4.Text = "clear"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Iphone", "Hauwei", "samsung", "sony"})
+        ComboBox1.Location = New Point(222, 52)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Lime
+        ClientSize = New Size(800, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
